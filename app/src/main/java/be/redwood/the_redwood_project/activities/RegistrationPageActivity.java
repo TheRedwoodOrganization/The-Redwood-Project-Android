0d358b9d09dc86331f7cdc375731566754d43cce
Noor Anthonis <noor.anthonis@gmail.com>
2015-09-21 11:34:18 +0200

    first commit

---
+++ app/src/main/java/be/redwood/the_redwood_project/activities/RegistrationPageActivity.java
@@ -0,0 +1,16 @@
+package be.redwood.the_redwood_project.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import be.redwood.the_redwood_project.R;
+
+public class RegistrationPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_page);
+    }
+
+}
